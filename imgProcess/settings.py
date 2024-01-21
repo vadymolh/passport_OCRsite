66802edf138c8d633e883034f9e22f05162e0c09
Arsenii Stratiuk <stratuik.777@gmail.com>
2024-01-21 13:40:50 +0200

    Update looks

--- imgProcess/settings.py
+++ imgProcess/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_DIRS = [ BASE_DIR / "static"]
